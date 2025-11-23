ab8321a29b8be8d7102740e27d80ccbab3521bcb
Raul <teles.r.raul@gmail.com>
2025-11-23 02:42:04 -0300

    retractable menu on mobile

--- public/shared/header.js
+++ public/shared/header.js
@@ -8,10 +8,18 @@ export async function load_header() {
     try {
         const response = await fetch('/shared/header.html');
         const data = await response.text();
-        document.getElementById('header-placeholder').innerHTML = data;
+        document.getElementById('header-placeholder').outerHTML = data;
 
         const page_name = getPageName(window.location.href);
         document.getElementById(page_name+"-nav-img").src = "/assets/navbar/" + page_name + "_color.svg";
+
+        document.getElementById("menu-btn").addEventListener("click", function(e) {
+            const navbar = document.getElementById("navbar");
+            if (navbar.style.display === "flex")
+                navbar.style.display = "none";
+            else
+                navbar.style.display = "flex";
+        });
 
     } catch (error) {
         console.error('Error loading header:', error);
