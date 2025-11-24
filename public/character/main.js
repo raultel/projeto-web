7d0519b2c781b7180d74e7216c20cc747f828e82
Raul <teles.r.raul@gmail.com>
2025-11-23 23:01:42 -0300

    check for null fields on char

--- public/character/main.js
+++ public/character/main.js
@@ -10,6 +10,14 @@ function withBreaks(text) {
     return text.replace(/\n/g, "<br>");
 }
 
+function create_item_field(content)
+{
+    let div = document.createElement("div");
+    div.classList.add("item-field");
+    div.textContent = content;
+    return div;
+}
+
 function build_card(item) {
     let div = document.createElement("div");
     let buttons;
@@ -19,21 +27,41 @@ function build_card(item) {
         buttons = setup_admin_view(item, modal, form, model_name, build_card);
     }
 
-    div.insertAdjacentHTML("afterbegin", `
-    <div class="character-art">
-        <img src="${item.img_path}" alt="">
-    </div>
-    <div class="character-info">
-        <div class="item-field"><h1>${item.name}</h1></div>
-        <div class="item-field">Outros nomes:: ${item.aliases}</div>
-        <div class="item-field">Idade: ${item.age}</div>
-        <div class="item-field">Espécie: ${item.species}</div>
-        <div class="item-field">Gênero: ${item.gender}</div>
-        <div class="item-field">Origem: ${item.origin}</div>
-        <div class="item-field"><p>${item.description}</p></div>
-    </div>
-    `);
+    let character_art = document.createElement("div");
+    character_art.classList.add("character-art");
+        let character_img = document.createElement("img");
+        character_img.src = item.img_path;
+        character_art.appendChild(character_img)
+    div.appendChild(character_art);
 
+    let character_info = document.createElement("div");
+    character_info.classList.add("character-info");
+        let name_div = document.createElement("div");
+        name_div.classList.add("item-field");
+            let name = document.createElement("h1");
+            name.textContent = item.name;
+        name_div.appendChild(name);
+
+    character_info.appendChild(name_div);
+    console.log("aliasses:", item.aliases)
+    if (item.aliases !== "")
+        character_info.appendChild(create_item_field('Outros nomes: '+item.aliases));
+    if (item.age !== "" && item.age !== 0)
+        character_info.appendChild(create_item_field('Idade: '+item.age));
+    if (item.species !== "")
+        character_info.appendChild(create_item_field('Espécie: '+item.species));
+    if (item.gender !== "")
+        character_info.appendChild(create_item_field('Gênero: '+item.gender));
+    if (item.origin !== "")
+        character_info.appendChild(create_item_field('Origem: '+item.origin));
+    let desc_div = document.createElement("div");
+    desc_div.classList.add("item-field");
+        let description = document.createElement("p");
+        description.textContent = item.description;
+    desc_div.appendChild(description);
+    character_info.appendChild(desc_div);
+
+    div.appendChild(character_info);
     div.querySelector(".character-info").append(buttons);
 
     if (token)
