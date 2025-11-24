26db729988825271f83009501328a212fa4f8ed5
Raul <teles.r.raul@gmail.com>
2025-11-23 22:30:47 -0300

    responsivenes on nav bar

--- public/shared/header.js
+++ public/shared/header.js
@@ -11,15 +11,15 @@ export async function load_header() {
         document.getElementById('header-placeholder').outerHTML = data;
 
         const page_name = getPageName(window.location.href);
-        document.getElementById(page_name+"-nav-img").src = "/assets/navbar/" + page_name + "_color.svg";
+        document.getElementById(page_name+"-nav-img").src = "/assets/navbar/" + page_name + "_v_color.svg";
 
-        document.getElementById("menu-btn").addEventListener("click", function(e) {
-            const navbar = document.getElementById("navbar");
-            if (navbar.style.display === "flex")
-                navbar.style.display = "none";
-            else
-                navbar.style.display = "flex";
-        });
+        // document.getElementById("menu-btn").addEventListener("click", function(e) {
+        //     const navbar = document.getElementById("navbar");
+        //     if (navbar.style.display === "flex")
+        //         navbar.style.display = "none";
+        //     else
+        //         navbar.style.display = "flex";
+        // });
 
     } catch (error) {
         console.error('Error loading header:', error);
