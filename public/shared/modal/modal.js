1663ba3d7b63ab86edbeba2f8b3011e61b3e7c1c
Raul <teles.r.raul@gmail.com>
2025-11-24 21:03:40 -0300

    explanation modal

--- public/shared/modal/modal.js
+++ public/shared/modal/modal.js
@@ -3,21 +3,25 @@ const openBtn = document.getElementsByClassName("open-btn")[0];
 const closeBtn = modal.querySelector(".close");
 const form = document.getElementById("model-form");
 
+/* Mostra o modal/form se clicar em add */
 openBtn.onclick = () => {
     form.reset()
     modal.style.display = "flex";
     modal.dataset.value=-1;
 };
+
+
+/* Esconde/fecha o modal/form quando envia o form ou fecha ele */
 closeBtn.onclick = () => {
     modal.style.display = "none";
     modal.dataset.value=-1;
 };
-
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     modal.style.display = "none";
 });
 
+/* Clicar fora do modal fecha o form */
 window.addEventListener("click", function(e){
     if(e.target===modal)
         modal.style.display="none";
