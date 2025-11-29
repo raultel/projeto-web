da299696fdd89eaad391fc3e7d1ba1c8a07f4ce5
Raul <teles.r.raul@gmail.com>
2025-11-28 22:49:43 -0300

    modify create_user for security

--- create_user.js
+++ create_user.js
@@ -2,8 +2,9 @@ import User from "./models/user.model.js";
 import bcrypt from "bcryptjs";
 import sequelize from "./dbconfig.js"
 
-const password = "123"
-const email = "R4YMiX"
+const email = process.env.ADMIN_EMAIL;
+const password = process.env.ADMIN_PASSWORD;
+
 const hashedPassword = bcrypt.hashSync(password, 10);
 
 await sequelize.sync();
